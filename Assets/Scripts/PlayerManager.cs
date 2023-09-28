75275f01abf4eb82e8358138151a70d7c23e3f54
AbsurdReference <ethan@conville.com>
2023-09-28 14:52:00 -0500

    Added player Movement
    Character now moves, still needs to be able to jump and attack (will add event listeners on Friday for attack / be damaged)

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    InputManager inputManager;
+    PlayerLocomotion playerLocomotion;
+    private void Awake()
+    {
+        inputManager = GetComponent<InputManager>();
+        playerLocomotion = GetComponent<PlayerLocomotion>();
+    }
+
+    private void Update()
+    {
+        inputManager.HandleAllInputs();
+        playerLocomotion.HandleAllMovement();
+    }
+
+    private void FixedUpdate()
+    {
+      //  playerLocomotion.HandleAllMovement();
+    }
+}
